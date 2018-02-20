eb2cf7ebeadd88a7bb04c7cf58d93da5d4bcab62
estefanoshin <estefanoshin@yahoo.com.ar>
2018-02-20 07:32:02 -0300

    bootstrap added

--- inc/footer.php
+++ inc/footer.php
@@ -2,5 +2,9 @@
 	<footer id="footer">
 		Copyright ©2018 Soda casual fashion. Todos los derechos reservados | info@sodacasual.com.ar
 	</footer>
+
+	<script src="bootstrap/js/jquery-3.2.1.slim.min.js"></script>
+	<script src="bootstrap/js/popper.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
--- inc/header.php
+++ inc/header.php
@@ -2,20 +2,38 @@
 <html>
 <head>
 	<title>Header</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+
 	<link rel="stylesheet" type="text/css" href="css/header.css">
 
 </head>
 <body>
 	<header>
 		<figure>
-			<img src="img/background/head_logo.fw.png">
+			
 		</figure>
 
-		<nav>
-			<div id="navegador">
-				<a href="index.php?page=readArticulos">Articulos_OK</a>
-				<a href="index.php?page=readMovimientos">Movimientos_OK</a>
-				<a href="index.php?page=readTelas">Telas_OK</a>
-			</div>
+		<nav class="navbar navbar-expand-lg navbar-light">
+
+		  <a class="navbar-brand" href="index.php?page=readArticulos"><img class="logo" src="img/background/head_logo.fw.png"></a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarNav">
+		    <ul class="navbar-nav">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="index.php?page=readArticulos">Articulos <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="index.php?page=readMovimientos">Movimientos</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link disabled" href="index.php?page=readTelas">Telas</a>
+		      </li>
+		    </ul>
+		  </div>
+
 		</nav>
+
 	</header>
